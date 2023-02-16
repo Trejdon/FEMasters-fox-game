18225c3d4ddb4f907f48c9c9813af35b25c717a6
Levi Trejo <ltrejo32@gmail.com>
2023-02-15 18:05:46 -0600

    feat(game): game state and style css added

---
+++ src/init.js
@@ -0,0 +1,24 @@
+import gameState from "./gameState";
+
+const TICK_RATE = 1000;
+
+function init() {
+  console.log("starting game");
+
+  let nextTimeToTick = Date.now();
+
+  function nextAnimationFrame() {
+    const now = Date.now();
+
+    if (nextTimeToTick <= now) {
+      gameState.tick();
+      nextTimeToTick = now + TICK_RATE;
+    }
+
+    requestAnimationFrame(nextAnimationFrame);
+  }
+
+  requestAnimationFrame(nextAnimationFrame);
+}
+
+init();
