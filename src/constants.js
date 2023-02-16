10f4bdb92ea0a39d252b001e295f4db8622bb014
Levi Trejo <ltrejo32@gmail.com>
2023-02-16 10:48:16 -0600

    feat(Game): code complete game

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,16 @@
-export const TICK_RATE = 1000;
+export const TICK_RATE = 3000;
 export const ICONS = ["fish", "poop", "weather"];
-export const RAIN_CHANCE = 0.3;
+export const RAIN_CHANCE = 0.4;
 export const SCENES = ["day", "rain"];
+export const DAY_LENGTH = 60;
+export const NIGHT_LENGTH = 10;
+
+export const getNextHungerTime = (clock) => {
+  return Math.floor(Math.random() * 3 + 5 + clock);
+};
+export const getNextDieTime = (clock) => {
+  return Math.floor(Math.random() * 2 + 3 + clock);
+};
+export const getNextPoopTime = (clock) => {
+  return Math.floor(Math.random() * 3 + 7 + clock);
+};
